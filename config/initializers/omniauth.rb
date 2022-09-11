d4711e552a1dc347c506f4110efc50f51acf9a2f
pro-soichiro <dr.1987626@gmail.com>
2022-09-11 12:09:29 +0900

    OAuthの導入

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+	if Rails.env.development? || Rails.env.test?
+		provider :github, "Iv1.168ef6ea07d58780", "83bc263f432eb58ab4ee0e6a735285e0b73ba968"
+	else
+		provider :github,
+			Rails.application.credentials.github[:client_id],
+			Rails.application.credentials.github[:client_secret]
+	end
+end
