5e2d67410e5236934456756e1c4cf959b51b3dba
pro-soichiro <dr.1987626@gmail.com>
2022-09-19 15:40:25 +0900

    退会機能実装完了

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,24 @@
+class TicketsController < ApplicationController
+  def new
+    raise ActionController::RoutingError, "ログイン状態でTicketsController#newにアクセス"
+  end
+
+  def destroy
+    ticket = current_user.tickets.find_by!(event_id: params[:event_id])
+    ticket.destroy!
+    redirect_to event_path(params[:event_id]), notice: "このイベントの参加をキャンセルしました"
+  end
+  
+  def create
+    event = Event.find(params[:event_id])
+    @ticket = current_user.tickets.build do |t|
+      t.event = event
+      t.comment = params[:comment]
+    end
+    puts @ticket
+    if @ticket.save
+      redirect_to event, notice: "このイベントに参加表明しました"
+    end
+  end
+
+end
